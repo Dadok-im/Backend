cf54f899f077c03e94f3fb56e9f1b3518286569b
leeminjxun <th554751@naver.com>
2025-11-05 17:19:06 +0900

    feat: HealthCheckController 구현

--- src/main/java/com/ayu/dadokim/global/config/SecurityConfig.java
+++ src/main/java/com/ayu/dadokim/global/config/SecurityConfig.java
@@ -150,7 +150,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PUT, "/api/user/put").hasRole(UserRoleType.USER.name())
                         .requestMatchers(HttpMethod.DELETE, "/api/user/delete").hasRole(UserRoleType.USER.name())
 
-                        //.requestMatchers(HttpMethod.GET, "healthcheck").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/healthcheck").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/env").permitAll()
 
                         .anyRequest().authenticated()
                 );
